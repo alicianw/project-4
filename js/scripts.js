e550abdd113adaf959355981f4f11b7fe374fdbd
alicia <alicianyambu@gmail.com>
2019-11-08 16:43:45 +0300

    changed position of form

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function order(){
+    var size= document.getElementByID("size").value;
+    var many= parseInt (document.getElementByID("many").value);
+    var deliver= document.getElementByID("deliver").value;
+    var crust= document.getElementByID("crust").value;
+}
