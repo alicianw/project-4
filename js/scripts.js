a08d998fda2d3bedf2bc8bae8cac876f902aa962
alicia <alicianyambu@gmail.com>
2019-11-12 08:47:46 +0300

    added jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
-function order() {
-    var size= parseInt (document.getElementByID("size").value);
-    var deliver=parseInt (document.getElementByID("deliver").value);
-    var crust= parseInt (document.getElementByID("crust").value);
+// function order() {
+//     var size= parseInt (document.getElementByID("size").value);
+//     var deliver=parseInt (document.getElementByID("deliver").value);
+//     var crust= parseInt (document.getElementByID("crust").value);
 
-var total = size + deliver + crust;
+// var total = size + deliver + crust;
 
-document.getElementById("result").innerHTML = size + " " + deliver + " " + crust + " " + total;
-}
+// return document.getElementById("total").innerHTML = size + " " + deliver + " " + crust + " " + total;
+
+// } 
+
+$(document).ready(function(){
+    $("#btn_order").click(function(event){
+    let pizzaFlavour =$("#name option:selected").val();
+    let pizzaSize =$("#size option:selected").val();
+    let pizzaDeliver =$("#deliver option:selected").val();
+    let pizzaCrust =$("#crust option:selected").val();
+    
+    })
+});
