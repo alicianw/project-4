4d05b748ad5b4cda723c4dddc8da4c1d5689abeb
alicia <alicianyambu@gmail.com>
2019-11-12 09:54:21 +0300

    added output table

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,39 @@
 // return document.getElementById("total").innerHTML = size + " " + deliver + " " + crust + " " + total;
 
 // } 
+let total=0;
 
 $(document).ready(function(){
+    function Eat(flavour, size, deliver, crust,topping,total){
+        this.flavour =flavour;
+        this.size =size;
+        this.deliver =deliver;
+        this.crust =crust;
+        this.total =total;
+        this.topping =topping;
+    }
+
     $("#btn_order").click(function(event){
-    let pizzaFlavour =$("#name option:selected").val();
-    let pizzaSize =$("#size option:selected").val();
-    let pizzaDeliver =$("#deliver option:selected").val();
-    let pizzaCrust =$("#crust option:selected").val();
+    let pizzaFlavour =parseInt($("#flvr option:selected").val());
+    let pizzaSize =parseInt($("#size option:selected").val());
+    let pizzaDeliver =parseInt($("#deliver option:selected").val());
+    let pizzaCrust =parseInt($("#crust option:selected").val());
+    let pizzaTopping=[];
+
+    $.each($("input[name='toppings']:checked"), function() {
+        pizzaTopping.push($(this).val());
+      });
     
+      let total_topping = pizzaTopping.length * 80;
+      total= pizzaSize+pizzaCrust+pizzaDeliver+pizzaTopping;
+
+    $("#pFlavour").html($("#flvr option:selected").val());
+    $("#pCrust").html($("#crust option:selected").val());
+    $("#pSize").html($("#size option:selected").val());
+    $("#pDeliver").html($("#deliver option:selected").val());
+    $("#pTopping").html(pizzaTopping.join(","));
+    $("#total").html(total);
+
+    event.preventDefault();
     })
 });
